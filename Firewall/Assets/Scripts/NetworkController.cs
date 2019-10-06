587c7944cef95c73533fcfdcc1cee4409cfbc9eb
unknown <orthogonaltrajectory@gmail.com>
2019-10-06 15:59:36 -0800

    Photon API integration with application key

---
+++ Firewall/Assets/Scripts/NetworkController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Photon.Pun;
+
+public class NetworkController : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start() {
+        // Open up a connection to Photon server host
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster() {
+        Debug.Log("Application connected to " + PhotonNetwork.CloudRegion + " Photon server!");
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
