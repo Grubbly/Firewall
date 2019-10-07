05e0a066fdcec531f6169d76f9dd4434f43a2895
unknown <orthogonaltrajectory@gmail.com>
2019-10-07 02:43:29 -0800

    Synchronized network animations and transforms

--- Firewall/Assets/Scripts/Player/PhotonPlayer.cs
+++ Firewall/Assets/Scripts/Player/PhotonPlayer.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 using Photon.Pun;
 using System.IO;
+using Photon.Pun.Demo.PunBasics;
 
 public class PhotonPlayer : MonoBehaviour
 {
@@ -13,6 +14,10 @@ public class PhotonPlayer : MonoBehaviour
     void Start()
     {
         photonView = GetComponent<PhotonView>();
+        InstantiateAvatar();
+    }
+
+    void InstantiateAvatar() {
         int spawnPointNum = Random.Range(0, GameSetup.gameSetup.spawnPoints.Length);
 
         // Instantiate Avatar
@@ -21,6 +26,23 @@ public class PhotonPlayer : MonoBehaviour
             GameSetup.gameSetup.spawnPoints[spawnPointNum].position,
             GameSetup.gameSetup.spawnPoints[spawnPointNum].rotation,
             0);
+            InstantiateCamera();
+        }
+    }
+
+    void InstantiateCamera() {
+        CameraWork _cameraWork = avatar.GetComponent<CameraWork>();
+
+        if (_cameraWork != null)
+        {
+            if (photonView.IsMine)
+            {
+                _cameraWork.OnStartFollowing();
+            }
+        }
+        else
+        {
+            Debug.LogError("<Color=Red><a>Missing</a></Color> CameraWork Component on playerPrefab.", this);
         }
     }
 }
