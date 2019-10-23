5235217edb2ada957d22e8c215b6e6a374007f18
unknown <orthogonaltrajectory@gmail.com>
2019-10-23 00:18:21 -0800

    prototype terminal and firewall

---
+++ Firewall/Assets/Scripts/Gameplay/Firewall.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Firewall : MonoBehaviour
+{
+    public bool toggleWall = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void enable() {
+        gameObject.SetActive(true);
+    }
+
+    public void disable() {
+        gameObject.SetActive(false);
+    }
+
+    private void Update() {
+        if(toggleWall) {
+            enable();
+        }
+        else if(!toggleWall) {
+            disable();
+        }
+    }
+}
