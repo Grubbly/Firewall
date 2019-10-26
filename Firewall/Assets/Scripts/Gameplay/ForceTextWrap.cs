1197d6cbd3141ba507f7b9ecef99a6e31334612a
unknown <orthogonaltrajectory@gmail.com>
2019-10-25 19:42:09 -0800

    Console menu - multiplayer implementation

---
+++ Firewall/Assets/Scripts/Gameplay/ForceTextWrap.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ForceTextWrap : MonoBehaviour
+{
+    TextMeshProUGUI TMP;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TMP = GetComponent<TextMeshProUGUI>();
+        TMP.enableWordWrapping = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!TMP.enableWordWrapping) {
+            TMP.enableWordWrapping = true;
+        }
+    }
+
+
+}
--- Firewall/Assets/Scripts/Gameplay/Terminal.cs
+++ Firewall/Assets/Scripts/Gameplay/Terminal.cs
@@ -1,10 +1,77 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
 
-public class Terminal : MonoBehaviour
+public class Terminal : MonoBehaviourPun, IPunOwnershipCallbacks
 {
+    [SerializeField]
+    GameObject console;
+    public bool activeElsewhere = false;
+
+    private void Awake() {
+        PhotonNetwork.AddCallbackTarget(this);
+        console.SetActive(false);
+    }
+
+    private void OnDestroy() {
+        PhotonNetwork.RemoveCallbackTarget(this);
+
+        if(console) {
+            console.SetActive(false);
+        }
+    }
+
+    public void OnOwnershipRequest(PhotonView targetView, Player requestingPlayer) {
+        if(targetView != base.photonView) {
+            return;
+        }
+        if(activeElsewhere) {
+            Debug.Log("Console controlled by other player -- REJECTING REQUEST");
+            return;
+        }
+
+        Debug.Log("TargetView: " + targetView + " Player: " + requestingPlayer + " is requesting ownership");
+
+        base.photonView.TransferOwnership(requestingPlayer);
+    }
+
+    public void OnOwnershipTransfered(PhotonView targetView, Player previousOwner) {
+        if(targetView != base.photonView) {
+            return;
+        }
+
+        photonView.RPC("RPC_SetConsoleState", RpcTarget.AllBuffered, true);
+        console.SetActive(true);  
+    }
+
     private void OnTriggerEnter(Collider other) {
         Debug.Log(other.gameObject + " entered terminal");
+
+        PhotonView view = other.GetComponent<PhotonView>();
+
+        if(view) {
+            if(view.IsMine) {
+                photonView.RequestOwnership();
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        
+        PhotonView view = other.GetComponent<PhotonView>();
+        
+        if(view) {
+            if(view.IsMine) {
+                console.SetActive(false);
+                photonView.RPC("RPC_SetConsoleState", RpcTarget.AllBuffered, false);
+            }
+        }
+    }
+
+    [PunRPC]
+    public void RPC_SetConsoleState(bool state) {
+        activeElsewhere = state;
     }
 }
