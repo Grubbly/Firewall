c0e55a52aea8dbfa8fff74fda53427a703b57934
unknown <orthogonaltrajectory@gmail.com>
2019-10-29 18:00:18 -0800

    Spend points to shoot if tag != malware

--- Firewall/Assets/Scripts/Gameplay/PointsManager.cs
+++ Firewall/Assets/Scripts/Gameplay/PointsManager.cs
@@ -10,6 +10,18 @@ public class PointsManager : MonoBehaviour
 
     public float rate = 0.01f;
 
+    public void spendPoints(float pointsToSpend) {
+        points -= pointsToSpend;
+    }
+
+    public void setRate(float newRate) {
+        rate = newRate;
+    }
+
+    public float getPoints() {
+        return points;
+    }
+
     private void OnGUI() {
         GUI.Box(new Rect(0,0,100,25), "Points: " + points.ToString("0.00"));
     }
